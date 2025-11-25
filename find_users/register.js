09de39438c26cc1e6df793f8b78ccf8001ff33e0
ganggmn <sk8292@naver.com>
2025-11-25 15:31:44 +0900

    sss

---
+++ find_users/register.js
@@ -0,0 +1,4 @@
+document.getElementById("requestBtn").addEventListener("click", () => {
+    const verifyBox = document.querySelector(".verify-area");
+    verifyBox.classList.remove("hidden");   // 인증창 표시
+});
